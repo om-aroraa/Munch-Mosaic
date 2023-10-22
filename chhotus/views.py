a0bd1d0981616f4ddd0ed61af70cd48108e15850
om-aroraa <120252758+om-aroraa@users.noreply.github.com>
2023-10-22 16:45:01 +0530

    started django ,yaya

--- chhotus/views.py
+++ chhotus/views.py
@@ -1,41 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from home.models import *
+
 
 def navbar(request):
     return render(request, 'navbar.html')
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 def beverages(request):
     return render(request, 'beverages.html')
 
+
 def snacks(request):
     return render(request, 'snacks.html')
+
 
 def desserts(request):
     return render(request, 'desserts.html')
 
+
 def cakes(request):
-    return render(request, 'cakes.html')
+    cakes = Product.objects.filter(subcategory="waffles")
+    cakes = list(cakes)
+    return render(request, 'cakes.html', {'cakes': cakes})
+
 
 def cart(request):
     return render(request, 'cart.html')
 
+
 def login(request):
     return render(request, 'login.html')
+
 
 def register(request):
     return render(request, 'register.html')
 
+
 def checkout(request):
     return render(request, 'checkout.html')
+
 
 def footer(request):
     return render(request, 'footer.html')
 
+
 def cursor(request):
     return render(request, 'cursor.html')
 
+
 def adminmenu(request):
-    return render(request, 'adminmenu.html')
+    if request.method == "POST":
+        data = request.POST
+        productName = data.get('productName')
+        price = data.get('price')
+        description = data.get('description')
+        image = request.FILES.get('image')
+        category = data.get('category')
+        subcategory = data.get('subcategory')
+
+        Product.objects.create(productName=productName,
+            price=price, 
+            description=description, 
+            image=image, 
+            category=category, 
+            subcategory=subcategory
+            )
+        
+        return redirect('/adminmenu')
+    else:
+        return render(request, 'adminmenu.html')
--- home/models.py
+++ home/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model):
+    product_id = models.AutoField
+    productName = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="static/imgs", default="")
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
